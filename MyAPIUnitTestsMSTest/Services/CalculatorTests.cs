414bd35f9373596cd80faf2153f746135ac2773b
RevProg <revavg93@gmail.com>
2024-08-08 15:48:35 +0200

    Adding MSTest project (#2)
    Co-authored-by: Arkadii Hryshko <ahryshko@microsoft.com>

---
+++ MyAPIUnitTestsMSTest/Services/CalculatorTests.cs
@@ -0,0 +1,55 @@
+﻿using MyAPI.Services;
+
+namespace MyAPIUnitTestsMSTest.Services;
+
+[TestClass]
+public class CalculatorTests
+{
+    private readonly Calculator _calculator;
+
+    public CalculatorTests()
+    {
+        _calculator = new Calculator();
+    }
+
+    [TestMethod]
+    public void Add_ValidNumbersFact_Success()
+    {
+        var actualResult = _calculator.Add(3, 5);
+
+        Assert.AreEqual(8, actualResult);
+    }
+
+    [TestMethod]
+    [DataRow(1, 2, 3)]
+    [DataRow(-5, 2, -3)]
+    [DataRow(1555, 232, 1787)]
+    public void Add_ValidNumbersTheory_Success(int first, int second, int expectedResult)
+    {
+        var actualResult = _calculator.Add(first, second);
+
+        //actualResult.Should().Be(expectedResult);
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+
+    [TestMethod]
+    [DataRow(1, 3, 3)]
+    [DataRow(5, 8, 40)]
+    public void Multiply_ValidNumbers_Success(int first, int second, int expectedResult)
+    {
+        var actualResult = _calculator.Multiply(first, second);
+
+        //actualResult.Should().Be(expectedResult);
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+
+    [TestMethod]
+    [DataRow(30, InputType.Degrees, 0.5)]
+    [DataRow(Math.PI / 2, InputType.Radians, 1)]
+    public void Sin_ValidInput_CorrectResult(double x, InputType type, double expectedResult)
+    {
+        var actualResult = _calculator.Sin(x, type);
+
+        Assert.AreEqual(expectedResult, actualResult, 5);
+    }
+}
---
+++ MyAPIUnitTestsMSTest/Services/NonPublicMembersTests.cs
@@ -0,0 +1,76 @@
+﻿using Moq.Protected;
+using MyAPI.Services;
+
+namespace MyAPIUnitTestsMSTest.Services;
+
+[TestClass]
+public class NonPublicMembersTests
+{
+    [TestMethod]
+    public void GetValue_NewMethod_Success()
+    {
+        var myService = new MyServiceTest();
+        Assert.AreEqual(25, myService.GetValue());
+    }
+
+    [TestMethod]
+    public void GetInternalValue_InternalVisibleTo_Success()
+    {
+        var myService = new MyService();
+        Assert.AreEqual(20, myService.GetInternalValue());
+    }
+
+    [TestMethod]
+    public void CalculateValue_ProtectedMockV1_Success()
+    {
+        var myServiceMock = new Mock<MyService>();
+
+        myServiceMock
+            .Setup(o => o.CalculateValue())
+            .CallBase();
+
+        myServiceMock
+            .Protected()
+            .Setup<int>("GetValue")
+            .Returns(5);
+
+        var actualValue = myServiceMock.Object.CalculateValue();
+
+        Assert.AreEqual(10, actualValue);
+    }
+
+    [TestMethod]
+    public void CalculateValue_ProtectedMockV2_Success()
+    {
+        var myServiceMock = new Mock<MyService>();
+
+        myServiceMock
+            .Setup(o => o.CalculateValue())
+            .CallBase();
+
+        myServiceMock
+            .Protected()
+            .As<IMyServiceTest>()
+            .Setup(o => o.GetValue())
+            .Returns(5)
+            .Verifiable(Times.Once);
+
+        var actualValue = myServiceMock.Object.CalculateValue();
+
+        Assert.AreEqual(10, actualValue);
+    }
+}
+
+internal class MyServiceTest : MyService
+{
+    // Override access modifer with new keyword
+    public new int GetValue()
+    {
+        return base.GetValue();
+    }
+}
+
+internal interface IMyServiceTest : IMyService
+{
+    int GetValue();
+}
