c8675164e3ffe3cbbbb6396355e8a0dfc46839a3
Arkadii Hryshko <ahryshko@microsoft.com>
2024-08-14 19:44:57 +0200

    AutoFixture samples

---
+++ MyAPI/Models/SomeData.cs
@@ -0,0 +1,10 @@
+﻿namespace MyAPI.Models;
+
+public class SomeData
+{
+    public Guid Guid { get; set; }
+
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+}
---
+++ MyAPIUnitTestsMSTest/AutoFixtureSamples.cs
@@ -0,0 +1,19 @@
+﻿using AutoFixture;
+using MyAPI.Models;
+
+namespace MyAPIUnitTestsMSTest;
+
+[TestClass]
+public class AutoFixtureSamples
+{
+    [TestMethod]
+    public void SampleTest()
+    {
+        var fixture = new Fixture();
+        var model = fixture.Create<SomeData>();
+
+        model.Should().NotBeNull();
+        model.Guid.Should().NotBeEmpty();
+        model.Name.Should().NotBeEmpty();
+    }
+}
